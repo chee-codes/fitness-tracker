4d41ef1190d3f82dcebba48d21c37e31e1458300
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2021-02-20 19:34:59 -0600

    Correct typing error

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const Workout = require("../models/Workout");
 
 router.get("/api/workouts", (req, res) => {
   Workout.aggregate([
-    { $addFields: { totalDuration: { $sum: "exercises.duration" } } },
+    { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
   ])
     .then((data) => res.json(data))
     .catch((err) => res.json(err));
