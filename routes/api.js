d6529662ea2265f6fa9ef3ee1e48866b2fccae10
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2021-02-20 18:05:58 -0600

    Update api routes

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,14 @@
 const router = require("express").Router();
-const db = require("../models");
+const Workout = require("../models/Workout");
 
 router.get("/api/workouts", (req, res) => {
-  db.Workout.find({})
+  Workout.find({})
     .then((data) => res.json(data))
     .catch((err) => res.json(err));
 });
 
 router.post("/api/workouts", (req, res) => {
-  db.Workout.create({})
+  Workout.create({})
     .then((data) => {
       console.log(data);
       res.json(data);
@@ -17,7 +17,7 @@ router.post("/api/workouts", (req, res) => {
 });
 
 router.put("/api/workouts/:id", (req, res) => {
-  db.Workout.findByIdAndUpdate(
+  Workout.findByIdAndUpdate(
     { _id: req.params.id },
     { $push: { exercises: req.body } },
     { new: true }
@@ -27,7 +27,7 @@ router.put("/api/workouts/:id", (req, res) => {
 });
 
 router.get("/api/workouts/range", (req, res) => {
-  db.Workout.find(req.body)
+  Workout.find(req.body)
     .then((data) => res.json(data))
     .catch((err) => res.json(err));
 });
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 app.use(require("./routes/html"));
 app.use(require("./routes/api"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
